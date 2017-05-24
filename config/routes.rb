543f5dacd2c43e0ae4ab8bfb2b4b346ea1e28a15
waleed arshad <waleed.arshad@virtualforce.io>
2017-05-24 20:10:05 +0500

    default routes to user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :plants
-  root to: 'visitors#index'
+  devise_scope :user do
+      authenticated  do
+        root to: 'visitors#index'
+      end
+
+      unauthenticated do
+        root to: 'devise/sessions#new', as: 'unauthenticated_root'
+      end
+    end  
+
+
+  # root to: 'visitors#index'
   devise_for :users
   resources :users
 end
