409911e64d6da33831170b03e5bb9821591915b1
waleedarshad <waleedarshad72@gmail.com>
2017-05-28 18:56:16 +0500

    User Dashboard added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,10 @@ class VisitorsController < ApplicationController
 	def index
 		if current_user.admin
 			redirect_to '/admin'
+		else
+			@plants = current_user.plants
+			@plant_with_material = current_user.plants.includes(:material_requirements)
 		end
+
 	end
 end
