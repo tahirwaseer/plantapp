7c0d49056cd62d5286d55796912e5a0a36c27f02
waleedarshad <waleedarshad72@gmail.com>
2017-06-02 01:14:46 +0500

    User dashboard filter added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -5,6 +5,22 @@ class VisitorsController < ApplicationController
 		else
 			@plants = current_user.plants
 			@plant_with_material = current_user.plants.includes(:material_requirements)
+
+			if params[:plant].present?
+				begin
+					@plants = current_user.plants
+				  @plant_with_material = current_user.plants.where(:id => params[:plant]).includes(:material_requirements)
+					
+				rescue 
+					@plants = current_user.plants
+				  @plant_with_material = @plants.includes(:material_requirements)
+
+				end
+			else
+				@plants = current_user.plants
+				  @plant_with_material = @plants.includes(:material_requirements)
+			end
+
 		end
 
 	end
