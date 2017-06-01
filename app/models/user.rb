b5e43eabc70f36b84769598519f11c187867606e
Tahir Waseer <tahirwaseer@gmail.com>
2017-06-02 00:01:15 +0500

    user activation check added based on the dates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :plants
+
+  # check to see if a user is active or not and deny login if not
+ def active_for_authentication?
+   super && effective_start_date.to_time.to_i <= Time.now.to_i && Time.now.to_i <= effective_end_date.to_time.to_i 
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170525150552) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "material_requirements", force: :cascade do |t|
     t.integer  "plant_id"
     t.date     "order_date"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170525150552) do
     t.datetime "inventory_run_out_time"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.index ["plant_id"], name: "index_material_requirements_on_plant_id"
+    t.index ["plant_id"], name: "index_material_requirements_on_plant_id", using: :btree
   end
 
   create_table "plants", force: :cascade do |t|
@@ -41,8 +44,8 @@ ActiveRecord::Schema.define(version: 20170525150552) do
   create_table "plants_users", id: false, force: :cascade do |t|
     t.integer "user_id",  null: false
     t.integer "plant_id", null: false
-    t.index ["plant_id", "user_id"], name: "index_plants_users_on_plant_id_and_user_id"
-    t.index ["user_id", "plant_id"], name: "index_plants_users_on_user_id_and_plant_id"
+    t.index ["plant_id", "user_id"], name: "index_plants_users_on_plant_id_and_user_id", using: :btree
+    t.index ["user_id", "plant_id"], name: "index_plants_users_on_user_id_and_plant_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,8 +65,9 @@ ActiveRecord::Schema.define(version: 20170525150552) do
     t.date     "effective_start_date"
     t.date     "effective_end_date"
     t.boolean  "admin",                  default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "material_requirements", "plants"
 end
