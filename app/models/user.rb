22cee6f8fb9eb9b2e0ddf3ebe0c87e436e685564
Tahir Waseer <tahirwaseer@gmail.com>
2017-06-07 23:26:36 +0500

    forgot password link removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :plants
 
